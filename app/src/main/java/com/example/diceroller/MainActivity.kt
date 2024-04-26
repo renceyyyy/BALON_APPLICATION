ff2399462d2ce3f77ca4d928ee62f59adadb85a4
quica <99055177+Rencey19@users.noreply.github.com>
2024-04-26 08:49:30 +0800

    First Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DiceRollerApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    Image(
+        painter = painterResource(id = R.drawable.cart),  // ito naman pong code na ito ay para sa background ng application
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentScale = ContentScale.FillWidth
+    )
+    Text(
+        text = "Color Game",
+        fontSize = 31.sp,
+        modifier = Modifier
+            .padding(16.dp)
+    )
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {  // dito po sa code na ito po ay para sa  mga image
+    var result by remember { mutableIntStateOf( 1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.blue
+        2 -> R.drawable.green
+        3 -> R.drawable.pink
+        4 -> R.drawable.red
+        6 -> R.drawable.violet
+        else -> R.drawable.yellow
+    }
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+
+        Button(
+            onClick = { result = (1..6).random() }, // and dito naman po sa code na ito para mag randomize ang mga image at hindi sunod sunod ang maging output ng color game para po ramdomize sya.
+        ) {
+            Text(text = stringResource(R.string.roll), fontSize = 14.sp)
+        }
+    }
+}
